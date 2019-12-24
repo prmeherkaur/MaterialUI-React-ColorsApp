d1c78e2365f4de65cb072e34de1ea97743f63b7f
Prmeher Kaur <prmeherkaur@gmail.com>
2019-12-24 19:54:16 +0530

    Creating basic pallete outline

---
+++ colors-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pallete from './Pallete';
+import seedColors from './seedColors';
+
+function App() {
+  return (
+    <div>
+      <Pallete {...seedColors[4]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ colors-app/src/Pallete.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Pallete extends Component {
+    render() {
+        return (
+            <div className="Pallete">
+                {/* Header  */}
+                <div className="Pallete-colors">{/*Color boxes*/}</div>
+                {/* Footer  */}
+            </div>
+        )
+    }
+}
