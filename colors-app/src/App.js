dc487be39605be48003c26d00536b0d90664db4a
Prmeher Kaur <prmeherkaur@gmail.com>
2020-03-07 22:32:04 +0530

    Added basic React-routing

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 import Palette from "./Palette";
+import {Route,Switch} from "react-router-dom";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id===id;
+    }
+    );
+  }
   render() {
-    //console.log(generatePalette(seedColors[4]));
+   
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
+        <Route exact path="/palette/:id"
+        render={routeProps =>(
+          <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
