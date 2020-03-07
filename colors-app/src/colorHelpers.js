994eea715f3c230d18b8e1b6fb8996b1b76ec32f
Prmeher Kaur <prmeherkaur@gmail.com>
2020-03-07 19:43:08 +0530

    Added slider functionality to change shades using chroma-js

--- colors-app/src/colorHelpers.js
+++ colors-app/src/colorHelpers.js
@@ -2,23 +2,29 @@ import chroma from "chroma-js";
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 function generatePalette(starterPalette) {
-  let newPalette = {
+  //takes pallete as input and generates new pallete on basis of shade
+  let newPalette = { // new pallete to be returned
     paletteName: starterPalette.paletteName,
     id: starterPalette.id,
     emoji: starterPalette.emoji,
-    colors: {}
+    colors: {
+      /*
+      50:[];
+      100:[];
+      */
+    } //we're only going to change the colors
   };
   for (let level of levels) {
     newPalette.colors[level] = [];
   }
   for (let color of starterPalette.colors) {
-    let scale = getScale(color.color, 10).reverse();
+    let scale = getScale(color.color, 10).reverse(); //light-to-dark
     for (let i in scale) {
       newPalette.colors[levels[i]].push({
         name: `${color.name} ${levels[i]}`,
         id: color.name.toLowerCase().replace(/ /g, "-"),
         hex: scale[i],
-        rgb: chroma(scale[i]).css(),
+        rgb: chroma(scale[i]).css(), //to get rgb color
         rgba: chroma(scale[i])
           .css()
           .replace("rgba", "rgba")
@@ -38,11 +44,12 @@ function getRange(hexColor) {
     end
   ];
 }
-
+//color.darken(1.4)-color-white(end)
 function getScale(hexColor, numberOfColors) {
-  return chroma
+  //gives us scale of colors based on input givel
+  return chroma //wrapping in chroma 
     .scale(getRange(hexColor))
-    .mode("lab")
+    .mode("lab")//lightness-ab
     .colors(numberOfColors);
 }
 
