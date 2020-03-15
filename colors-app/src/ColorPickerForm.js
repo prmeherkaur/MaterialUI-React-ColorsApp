e6d2fafb2a19d1ca4739be995f534edc208ef847
Prmeher Kaur <prmeherkaur@gmail.com>
2020-03-15 22:46:26 +0530

    Added luminance effects

--- colors-app/src/ColorPickerForm.js
+++ colors-app/src/ColorPickerForm.js
@@ -57,7 +57,11 @@ class ColorPickerForm extends Component{
                     onChangeComplete={this.updateCurrentColor}
                     className={classes.picker}
                 />
-          <ValidatorForm onSubmit={this.handleSubmit} ref='form'>
+          <ValidatorForm 
+            onSubmit={this.handleSubmit} 
+            ref='form'
+            instantValidate={false}
+          >
             <TextValidator
               value={this.state.newColorName}
               name='newColorName'
