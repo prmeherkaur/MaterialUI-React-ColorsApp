d3df48a7df32fffabcba7a748411069786f5b5af
Prmeher Kaur <prmeherkaur@gmail.com>
2020-07-27 13:14:53 +0530

    Updates

---
+++ precache-manifest.2f34a8d81e215e43b9908a6e991df067.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7008d32f5ab3cafe04a50e241c5d022",
+    "url": "/MaterialUI-React-ColorsApp/index.html"
+  },
+  {
+    "revision": "8a19138996b8b38673ca",
+    "url": "/MaterialUI-React-ColorsApp/static/css/2.36bfdc33.chunk.css"
+  },
+  {
+    "revision": "f6541c19402346005cfa",
+    "url": "/MaterialUI-React-ColorsApp/static/css/main.2f0bbd55.chunk.css"
+  },
+  {
+    "revision": "8a19138996b8b38673ca",
+    "url": "/MaterialUI-React-ColorsApp/static/js/2.eb0ce4ed.chunk.js"
+  },
+  {
+    "revision": "8c89aadc156c8b27e5cdc9afc12e7cb5",
+    "url": "/MaterialUI-React-ColorsApp/static/js/2.eb0ce4ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6541c19402346005cfa",
+    "url": "/MaterialUI-React-ColorsApp/static/js/main.b6c19e5a.chunk.js"
+  },
+  {
+    "revision": "642c0989ceb30f5b7f07",
+    "url": "/MaterialUI-React-ColorsApp/static/js/runtime-main.b423fded.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/MaterialUI-React-ColorsApp/static/media/bg.77428cd4.svg"
+  }
+]);
